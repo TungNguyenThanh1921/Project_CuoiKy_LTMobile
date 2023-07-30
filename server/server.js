c187c009abc9130293b424e778d2f55fe6782442
quangpham2803 <108970825+quangpham2803@users.noreply.github.com>
2023-07-30 21:43:49 +0700

    update sendImage + uiChat

--- server/server.js
+++ server/server.js
@@ -5,24 +5,30 @@ const wss = new WebSocket.Server({ port: 9090 });
 wss.on('connection', (ws) => {
   console.log('A client connected.');
 
-  // Xử lý tin nhắn mới từ client
   ws.on('message', (message) => {
-    console.log('New message:', message);
+    console.log('New message received:', message);
 
-    // Broadcast tin nhắn mới tới tất cả các client khác
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-        console.log('Đã gửi tin nhắn cho các client khác');
-        
-      }
-    });
+    if (message instanceof Uint8Array) {
+      // Broadcast image data to all other clients
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    } else if (typeof message === 'string') {
+      // Broadcast text message to all other clients
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    } else {
+      console.log('Received unexpected data format.');
+    }
   });
 
-  // Xử lý sự kiện client đóng kết nối
   ws.on('close', () => {
     console.log('A client disconnected.');
-    
   });
 });
 
@@ -31,6 +37,7 @@ wss.on('listening', () => {
   console.log(`WebSocket server is running on ${address}:${port}`);
 });
 
+
 // Phần HTTP
 const express = require('express');
 const app = express();
@@ -38,6 +45,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { connectDB, queryDB } = require('./database');
 let pool = null; 
+
 // Middleware để kết nối cơ sở dữ liệu trước khi xử lý yêu cầu
 app.use((req, res, next) => {
   if (!pool) {
@@ -50,13 +58,13 @@ app.use((req, res, next) => {
 app.get('/data', async (req, res) => {
   try {
     // Thực hiện các truy vấn lấy dữ liệu từ cơ sở dữ liệu
-    const result = await queryDB('SELECT * FROM PRODUCT');
-    const chatSamples = result.map((row) => row.ProductName);
+    const result = await queryDB('SELECT * FROM Users');
+    console.log(result);
+    const chatSamples = result.map((row) => row.username);
     res.json({ chat_samples: chatSamples });
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Error fetching data' });
-    console.log('loi gui api');
   }
 });
 
@@ -77,9 +85,6 @@ app.get('/getIP', (req, res) => {
 	console.log(ipAddress)
 });
 
-
 const server = app.listen(8080, () => {
   console.log('HTTP server is running on port 8080');
 });
-
-
